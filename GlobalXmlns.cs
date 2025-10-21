16d1407a9586c290347b5bfec879d8e8fdfddd6b
ChrystalFSC <bcs23090006@student.uts.edu.my>
2025-10-21 22:17:29 +0800

    first commit

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LabTask2")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "LabTask2.Pages")]
